fefaf9ec4490be7b79f26d73c9fdc13ac34d7b2a
GitHubProo <42809244+GitHubProo@users.noreply.github.com>
2022-05-12 19:06:39 +0300

    responsive web
    grid

---
+++ src/AppDinamic.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+import './AppDinamic.css';
+
+
+const AppDinamic = () => {
+
+    return (
+        <div  className={"wrapper"}>
+            <div className={"containerHome"}>
+                <img src={"photoHeader.png"}/>
+            </div>
+            <div className={"containerAboutMe"}>
+                <div className={"aboutMe_GoalsValuesHobbies"}>
+                    <p className={"aboutMe"}>GOALS</p>
+                    Life is like a combination lock; your job is to find the numbers,
+                    in the right orders, so you can have anything you want. Brian Tracy ”
+                </div>
+                <div className={"aboutMe_GoalsValuesHobbies"}>
+                    <p className={"aboutMe"}>VALUES</p>
+                    I enjoy my work
+
+                </div>
+                <div className={"aboutMe_GoalsValuesHobbies"}>
+                    <p className={"aboutMe"}>HOBBIES</p>
+                    I love board games
+                </div>
+            </div>
+            <div className={"portfolioContent"}>
+                <div className={"hublish"}>
+                    <img src={"hublish.png"}/>
+                </div>
+                <div className={"llook"}>
+                    <img src={"llok.png"}/>
+                </div>
+                <div className={"greenHorn"}>
+                    <img src={"greenHorn.png"}/>
+                </div>
+                <div className={"digibarber"}>
+                    <img src={"digibarber.png"}/>
+                </div>
+                <div className={"organic"}>
+                    <img src={"organic.png"}/>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default AppDinamic;
+
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AppDinamic from './AppDinamic'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+      <AppDinamic />
   </React.StrictMode>,
   document.getElementById('root')
 );
